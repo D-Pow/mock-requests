62c8d043f143ee42cb4232ee60930374fc7158c5
D-Pow <djp460@nyu.edu>
2019-08-30 12:32:48 -0400

    Add base API for setting and getting mock URL-response mappings

--- src/RequestMock.js
+++ src/RequestMock.js
@@ -1,5 +1,42 @@
 const RequestMock = (function() {
-    return {};
+    /**
+     * @type {Object.<string, Object>} urlResponseMap - key (URL string) value (mock response) pairs for network mocks
+     */
+    let urlResponseMap = {};
+
+    /**
+     * Initialize the mock
+     *
+     * @param  {Object.<string, Object>} apiUrlResponseConfig - Config object containing URL strings as keys and respective mock response objects as values
+     */
+    function configure(apiUrlResponseConfig = {}) {
+        urlResponseMap = apiUrlResponseConfig;
+    }
+
+    /**
+     * Mock any network requests to the given URL using the given responseObject
+     * @param {string} url
+     * @param {Object} responseObject
+     */
+    function setMockUrlResponse(url, responseObject) {
+        urlResponseMap[url] = responseObject;
+    }
+
+    /**
+     * Get the mock response object associated with the passed URL
+     *
+     * @param {string} url
+     * @returns {Object} - Configured response object
+     */
+    function getResponse(url) {
+        return urlResponseMap[url];
+    }
+
+    return {
+        configure,
+        setMockUrlResponse,
+        getResponse
+    };
 })();
 
 export default RequestMock;
