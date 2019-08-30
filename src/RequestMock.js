0e46220d57f6b74faf1264c2cac6ef3457446222
D-Pow <djp460@nyu.edu>
2019-08-30 12:35:55 -0400

    Overwrite XMLHttpRequest with wrapper to return mock responses

--- src/RequestMock.js
+++ src/RequestMock.js
@@ -37,6 +37,69 @@ const RequestMock = (function() {
         return urlResponseMap[url];
     }
 
+    /**
+     * Overwrites the XMLHttpRequest function with a wrapper that
+     * mocks the readyState, status, statusText, and various other
+     * fields that depend on the status of the request, and applies
+     * the mock object response to the `xhr.response` field.
+     *
+     * The wrapper always marks the request as successful,
+     * e.g. status = 200 and statusText = 'OK'
+     */
+    function overwriteXmlHttpRequestObject() {
+        const OriginalXHR = XMLHttpRequest;
+
+        XMLHttpRequest = function() {
+            const xhr = new OriginalXHR();
+
+            return {
+                ...xhr,
+                get readyState() {
+                    return 4;
+                },
+                set readyState(val) {},
+                get status() {
+                    return 200;
+                },
+                set status(val) {},
+                get statusText() {
+                    return 'OK';
+                },
+                set statusText(val) {},
+                get responseUrl() {
+                    return xhr.url;
+                },
+                set responseUrl(val) {},
+                get response() {
+                    return urlResponseMap[xhr.url];
+                },
+                set response(val) {},
+                get responseText() {
+                    return JSON.stringify(urlResponseMap[xhr.url]);
+                },
+                set responseText(val) {},
+                get timeout() {
+                    return 0;
+                },
+                set timeout(val) {},
+                open: function(method, url, ...args) {
+                    xhr.url = url; // store URL for later access
+                    xhr.open(method, url, ...args);
+                },
+                set onreadystatechange(fn) {
+                    xhr.onreadystatechange = fn;
+                },
+                send: function() {
+                    xhr.onreadystatechange();
+                }
+            };
+        }
+    }
+
+    if (XMLHttpRequest) {
+        overwriteXmlHttpRequestObject();
+    }
+
     return {
         configure,
         setMockUrlResponse,
