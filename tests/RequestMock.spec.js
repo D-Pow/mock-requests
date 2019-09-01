9bbd49837d26c2f2d938fa2d36c0bc96ba69534a
D-Pow <djp460@nyu.edu>
2019-08-31 23:06:34 -0400

    Add tests for getting mock objects and ensuring mocks are returned in fetch/XHR
    ToDo: add tests for ensuring fetch/XHR is actually called for non-mocked URLs

--- tests/RequestMock.spec.js
+++ tests/RequestMock.spec.js
@@ -1,7 +1,69 @@
 import RequestMock from '../src/RequestMock';
 
+const mockUrl1 = 'https://example.com';
+const mockUrl2 = 'https://other.example/someApi';
+const mockUrl3 = '192.168.0.1';
+const mockUrl4 = 'http://google.com';
+const mockUrl5 = 'http://not.mocked/api';
+
+const mockConfig1 = {
+    [mockUrl1]: { data: 'myData' },
+    [mockUrl2]: { otherData: { someNestedField: 'myValue' }}
+};
+const mockConfig2 = {
+    [mockUrl3]: 'My dangling string response',
+    [mockUrl4]: '<!DOCTYPE html><head><meta http-equiv="content-type" content="text/html;charset=utf-8"><title>301 Moved</title></head><body><h1>301 Moved</h1>The document has moved<a HREF="http://www.google.com/">here</a>.</body></html>'
+};
+
 describe('RequestMock', () => {
-    it('should run', () => {
-        expect(typeof RequestMock.configure).toEqual('function');
+    beforeEach(() => {
+        RequestMock.clearAllMocks();
+    });
+
+    it('should configure URLs to mock in constructor and setter function', () => {
+        expect(RequestMock.getResponse(mockUrl1)).toBe(undefined);
+
+        RequestMock.configure(mockConfig1);
+        RequestMock.setMockUrlResponse(mockUrl3, mockConfig2[mockUrl3]);
+
+        expect(RequestMock.getResponse(mockUrl1)).toEqual(mockConfig1[mockUrl1]);
+        expect(RequestMock.getResponse(mockUrl3)).toEqual(mockConfig2[mockUrl3]);
+    });
+
+    it('should be able to parse different types of response bodies, including JSON and HTML', () => {
+        RequestMock.configure({ ...mockConfig1, ...mockConfig2 });
+
+        expect(typeof RequestMock.getResponse(mockUrl1)).toEqual(typeof {});
+        expect(typeof RequestMock.getResponse(mockUrl3)).toEqual(typeof '');
+        expect(typeof RequestMock.getResponse(mockUrl4)).toEqual(typeof '');
+    });
+
+    it('should overwrite the result of fetch for URLs mocked', async () => {
+        RequestMock.configure({ ...mockConfig1, ...mockConfig2 });
+
+        const mockFetchResponseJson = await fetch(mockUrl2).then(res => res.json());
+        const mockFetchResponseText = await fetch(mockUrl4).then(res => res.text());
+
+        expect(mockFetchResponseJson).toEqual(mockConfig1[mockUrl2]);
+        expect(mockFetchResponseText).toEqual(mockConfig2[mockUrl4]);
+    });
+
+    it('should overwrite the result of XMLHttpRequest for URLs mocked', async () => {
+        RequestMock.configure({ ...mockConfig1, ...mockConfig2 });
+
+        const mockXHR = new XMLHttpRequest();
+        mockXHR.open('GET', mockUrl2);
+        mockXHR.onreadystatechange = () => {
+            expect(mockXHR.response).toEqual(mockConfig1[mockUrl2]);
+        };
+        mockXHR.send();
+    });
+
+    it('should call fetch for URLs not mocked', async () => {
+        // TODO
+    });
+
+    it('should call fetch/XMLHttpRequest for URLs not mocked', async () => {
+        // TODO
     });
 });
