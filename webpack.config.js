b4ef529e70e084a031dc1cc8efe4c4ac069a9f45
D-Pow <djp460@nyu.edu>
2019-09-07 10:26:30 -0400

    Change output file name to match library name

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/index.js'
+        MockRequests: './src/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
