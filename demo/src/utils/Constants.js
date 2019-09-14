69a9bc32e0da0b2742c08fd2b08e4bcf5b95f038
D-Pow <djp460@nyu.edu>
2019-09-14 14:04:11 -0400

    Add MyAnimeList search URL

--- demo/src/utils/Constants.js
+++ demo/src/utils/Constants.js
@@ -1,5 +1,8 @@
 export const kitsuTitleSearchUrl = 'https://kitsu.io/api/edge/anime?filter[text]=';
+export const myAnimeListSearchUrl = 'https://myanimelist.net/anime.php?q=';
 
 export const getSearchUrl = (domainUrl, searchText) => domainUrl + encodeURIComponent(searchText);
 
 export const getKitsuTitleSearchUrl = searchText => getSearchUrl(kitsuTitleSearchUrl, searchText);
+
+export const getMyAnimeListSearchUrl = searchText => getSearchUrl(myAnimeListSearchUrl, searchText);
