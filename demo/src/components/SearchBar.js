4ae328f970671decfd58d7b1a3728eba8005680d
D-Pow <djp460@nyu.edu>
2019-09-14 18:57:15 -0400

    Add styling to search bar along with search icon

--- demo/src/components/SearchBar.js
+++ demo/src/components/SearchBar.js
@@ -4,10 +4,25 @@ function SearchBar(props) {
     const [ typedText, setTypedText ] = useState('');
 
     return (
-        <div>
-            <input type={'text'} value={typedText} onChange={e => setTypedText(e.target.value)} />
+        <div className={'row my-5'}>
+            <div className={'col-xs-12 mx-auto'}>
+                <div className={'input-group my-3'}>
+                    <input
+                        className={'form-control input-large'}
+                        type={'text'}
+                        placeholder={'e.g. "Kimi no na wa"'}
+                        value={typedText}
+                        onChange={e => setTypedText(e.target.value)}
+                    />
+                    <div className={'input-group-append'}>
+                        <button className={'btn btn-outline-secondary'}>
+                            <i className={'fas fa-search'} />
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default SearchBar;
