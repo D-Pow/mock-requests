20d3801863fc0167534604d2a8440ad476d9b5a9
D-Pow <djp460@nyu.edu>
2019-09-14 14:31:37 -0400

    Add basic title rendering
    Also convert to functional component since no real logic will be done here

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>Hello world</div>
-        );
-    }
+function App() {
+    const pageText = {
+        title: 'Anime search'
+    };
+
+    const renderedTitle = (
+        <div className={'row mt-5'}>
+            <div className={'col-xs-12 text-center mx-auto'}>
+                <h1>{pageText.title}</h1>
+            </div>
+        </div>
+    );
+
+    return (
+        <div className={'container'}>
+            <div className={'text-center mx-auto'}>
+                {renderedTitle}
+            </div>
+        </div>
+    );
 }
 
 export default App;
