18bedf4c4b765586ede458dc77885f7a1534fa4e
D-Pow <djp460@nyu.edu>
2019-09-14 20:27:23 -0400

    Add SearchResults component to display a list of <KitsuResultCard/> components

---
+++ demo/src/components/SearchResults.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import KitsuResultCard from 'components/KitsuResultCard';
+
+function SearchResults({ kitsuResults }) {
+    if (!kitsuResults) {
+        return '';
+    }
+
+    return (
+        <div className={'row my-5'}>
+            <div className={'col-xs-12 mx-auto'}>
+                <ul className={'list-unstyled'}>
+                    {kitsuResults.data.map(kitsuResult => {
+                        return (
+                            <li className={'media row w-75 mb-5 mx-auto'} key={kitsuResult.id}>
+                                <KitsuResultCard kitsuResult={kitsuResult} />
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+SearchResults.propTypes = {
+    kitsuResults: PropTypes.object
+};
+
+export default SearchResults;
