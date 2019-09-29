d964bde0b6e9ab1385c7dabd5334d1095f1bb8e5
D-Pow <djp460@nyu.edu>
2019-09-29 16:12:12 -0400

    Add 'library' config field
    So that the 'MockRequests' name is retained when run in the window

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
-        library: '[name]',
+        library: 'MockRequests',
         libraryTarget: 'umd'
     },
     module: {
