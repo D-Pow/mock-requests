346f45bcc48a90ffa4c4b36cf4fd7d669971bdd6
D-Pow <djp460@nyu.edu>
2019-10-20 17:50:33 -0400

    Add sample dynamic configuration for search queries
    This way, users can see both the static and dynamic configuration methods. Comment out the second (dynamic) method so that MockRequests.configure() isn't called twice

--- demo/mocks/MockConfig.js
+++ demo/mocks/MockConfig.js
@@ -1,4 +1,21 @@
 import MockRequests from 'mock-requests';
-import { searchMocksConfig } from './UrlResponseMappings';
+import {
+    searchMocksConfig,
+    dynamicSearchConfigFromQueries
+} from './UrlResponseMappings';
 
+/**
+ * The below two configuration methods are identical in that they map the known search
+ * queries to saved mock JSON responses. This illustrates two of the ways you can use MockRequests.
+ */
+
+/**
+ * 1. Configuring static URL-response object mappings
+ */
 MockRequests.configure(searchMocksConfig);
+
+/**
+ * 2. Configuring dynamic URL responses, in this case using the queryParamMap to
+ * determine which response to return
+ */
+// MockRequests.configureDynamicResponses(dynamicSearchConfigFromQueries);
