98614adeaeb383a16e58a8eeb34920d2b0d6235f
D-Pow <djp460@nyu.edu>
2020-02-07 14:31:26 -0500

    Improve JSDoc of resolve-mocks script
    When declaring an object with specified key names and value types inside the @returns tag of the JSDoc, the generated JSDoc document doesn't show these fields and simply shows 'Object' as the return type instead. Thus, extract the object structure to a typedef and add that in the @returns type

--- bin/resolve-mocks.js
+++ bin/resolve-mocks.js
@@ -23,18 +23,27 @@ function getResolvedPathAsArray(mockPath) {
 }
 
 /**
- * @module
+ * @module mock-requests/bin/resolve-mocks
+ */
+
+/**
+ * @typedef {Object} ResolvedMocks
+ * @property {string[]} include - Array containing mocks/ directory; spread it in `module.rules[jsLoader].include` webpack config field
+ * @property {string[]} entry - Array containing mock entry file; spread it in `entry` webpack config field
  */
 
 /**
  * Returns an object containing arrays of paths to pass into the webpack `module.rules[jsLoader].include`
  * and `entry` configuration fields. The returned arrays should be spread inside the arrays already
- * present in those configuration fields. Will return empty arrays if `activateMocks` is false to allow
- * for selective activation of mocks.
+ * present in those configuration fields.
+ *
+ * This will return empty arrays if `activateMocks` is false so that the user doesn't have to modify the respective
+ * webpack config fields when activating/deactivating mocks.
  *
  * @param {string} mocksDir - Directory containing all mock-related code. Only needed if transpiling JS code containing mocks.
  * @param {string} mockConfigFile - Entry JS file that configures all mocks.
- * @param {boolean} activateMocks - If mocks should be activated.
+ * @param {boolean} activateMocks - If mocks should be activated/injected into build or not.
+ * @returns {ResolvedMocks} - Object containing arrays of files to spread in `entry` and `include` webpack config fields
  */
 module.exports = function resolveMocks(mocksDir, mockConfigFile, activateMocks) {
     if (activateMocks) {
@@ -45,4 +54,4 @@ module.exports = function resolveMocks(mocksDir, mockConfigFile, activateMocks) 
         include: activateMocks ? getResolvedPathAsArray(mocksDir) : [],
         entry: activateMocks ? getResolvedPathAsArray(mockConfigFile) : []
     };
-}
+};
