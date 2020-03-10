8859f36ff7a86e11ebb78bd6f01710a1dde57bff
D-Pow <djp460@nyu.edu>
2020-03-10 10:39:05 -0400

    Reset jest state before each test
    Includes resetting the module itself (so that subsequent imports don't end up using the 'globalScope' var from the previous import call - a problem since MockRequests is only instantiated once), resetting of the global object name back to 'window', and resetting the XHR/fetch values in 'global'

--- tests/GlobalFieldVariabilities.spec.js
+++ tests/GlobalFieldVariabilities.spec.js
@@ -1,4 +1,19 @@
+const origGlobalName = 'window';
+const origGlobalObj = global[origGlobalName];
+const globalNameOptions = [ 'window', 'global', 'self' ];
+const OrigXHR = global.XMLHttpRequest;
+const origFetch = global.fetch;
+
 describe('Global field variabilities', () => {
+    beforeEach(() => {
+        jest.resetModules();
+        globalNameOptions.forEach(globalName => delete global[globalName]);
+
+        global[origGlobalName] = origGlobalObj;
+        global.XMLHttpRequest = OrigXHR;
+        global.fetch = origFetch;
+    });
+
     describe('MockRequests without async functions defined in `window`', () => {
         it('should not overwrite fetch/XMLHttpRequest if it is not defined in the browser', async () => {
             global.XMLHttpRequest = undefined;
