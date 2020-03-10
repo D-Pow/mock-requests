2acfb7bdb336c2e362106f4b9a546d42eae5334e
D-Pow <djp460@nyu.edu>
2020-03-10 10:41:22 -0400

    Add new describe() test block with util functions for verifying that MockRequests works correctly
    Will be used in subsequent commit to test that MockRequests still maintains static/dynamic functionality when changing the name of the global

--- tests/GlobalFieldVariabilities.spec.js
+++ tests/GlobalFieldVariabilities.spec.js
@@ -25,4 +25,77 @@ describe('Global field variabilities', () => {
             expect(MockRequestsWithoutAsync.OriginalXHR).toBe(undefined);
         });
     });
+
+    describe('global name differs from "window"', () => {
+        const mockUrl1 = 'https://example.com';
+        const mockUrl2 = 'https://other.example/someApi';
+        const mockUrl3 = '192.168.0.1';
+        const staticConfig = {
+            [mockUrl1]: { otherData: { someNestedField: 'myValue' }},
+            [mockUrl2]: '<!DOCTYPE html><head><meta http-equiv="content-type" content="text/html;charset=utf-8"><title>301 Moved</title></head><body><h1>301 Moved</h1>The document has moved<a HREF="http://www.google.com/">here</a>.</body></html>'
+        };
+        const dynamicConfig = {
+            [mockUrl3]: {
+                response: {
+                    data: ['a', 'b', 'c', 'd', 'e'],
+                    value: 7
+                },
+                dynamicResponseModFn: (request, response) => {
+                    response.data = response.data.concat(request.addLettersArray);
+                    response.value += request.valueModification;
+
+                    return response;
+                }
+            }
+        };
+
+        async function testStaticResponses(MockRequests) {
+            const mockFetchResponseJson = await fetch(mockUrl1).then(res => res.json());
+            const mockFetchResponseText = await fetch(mockUrl2).then(res => res.text());
+
+            expect(mockFetchResponseJson).toEqual(staticConfig[mockUrl1]);
+            expect(mockFetchResponseText).toEqual(staticConfig[mockUrl2]);
+        }
+
+        async function testDynamicResponses(MockRequests) {
+            const originalConfig = JSON.parse(JSON.stringify(dynamicConfig[mockUrl3].response));
+
+            const mockPayloadRound1 = {
+                addLettersArray: ['f', 'g'],
+                valueModification: 5
+            };
+            const modifiedResponseRound1 = await fetch(mockUrl3, {
+                body: JSON.stringify(mockPayloadRound1)
+            }).then(res => res.json());
+            const expectedResponseRound1 = {
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g'],
+                value: 12
+            };
+            expect(modifiedResponseRound1).toEqual(expectedResponseRound1);
+
+            const mockPayloadRound2 = {
+                addLettersArray: ['h', 'i', 'j'],
+                valueModification: -10
+            };
+            const modifiedResponseRound2 = await fetch(mockUrl3, {
+                body: JSON.stringify(mockPayloadRound2)
+            }).then(res => res.json());
+            const expectedResponseRound2 = {
+                data: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'],
+                value: 2
+            };
+            expect(modifiedResponseRound2).toEqual(expectedResponseRound2);
+
+            expect(dynamicConfig[mockUrl3].response).toEqual(originalConfig);
+        }
+
+        async function testMockRequestsWorks(MockRequests) {
+            MockRequests.clearAllMocks();
+            MockRequests.configure(staticConfig);
+            MockRequests.configureDynamicResponses(dynamicConfig, false);
+
+            await testStaticResponses(MockRequests);
+            await testDynamicResponses(MockRequests);
+        }
+    });
 });
