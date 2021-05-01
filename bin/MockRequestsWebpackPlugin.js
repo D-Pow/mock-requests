e65008e6de889c131ba1579910170a7cef3a19f4
D-Pow <djp460@nyu.edu>
2021-05-01 11:52:00 -0600

    Split plugin logic into separate functions for ease of understanding
    Rather than having long blocks of code all nested within the `hook.tap` callback, split them into separate functions for easier understanding.
    
    This will also make the plugin more configurable both with users' webpack config files and with extra plugin options, including:
    
    * User config: If the user passes in a function/promise into their webpack config's `entry` field, we can wait until it's processed before injecting mocks.
    * Plugin option: If the user wants to choose which `entry` object key to inject mocks into (rather than arbitrarily choosing the first one).
    * Plugin option: If the user wants to choose a `module.rule` to process mocks with.
    * Plugin option: If the user wants to toggle the injection of the mocks dir into `module.rule` on/off.

--- bin/MockRequestsWebpackPlugin.js
+++ bin/MockRequestsWebpackPlugin.js
@@ -33,6 +33,66 @@ class MockRequestsWebpackPlugin {
         return absPath;
     }
 
+    injectMocksIntoWebpackConfig(projectRootPath, moduleRules, entry) {
+        try {
+            const firstEntryName = Object.keys(entry)[0];
+            const firstEntryList = entry[firstEntryName].import;
+            const mockDirAbsPath = this.getAbsPath(projectRootPath);
+            const mockEntryAbsPath = this.getAbsPath(projectRootPath, true);
+
+            if (!mockDirAbsPath) {
+                throw new Error(`Could not find mock directory "${this.mocksDir}"`);
+            }
+
+            if (!mockEntryAbsPath) {
+                throw new Error(`Could not find mock entry file "${this.mockEntryFile}"`);
+            }
+
+            this.addMockEntryFileToConfigEntry(firstEntryList, mockEntryAbsPath);
+            this.addMockDirToModuleRule(moduleRules, mockDirAbsPath);
+        } catch (e) {
+            console.error('Error:', e.message);
+            console.error('Note:', this.pluginName, 'has only been verified for webpack@>=5. Webpack runtime issues may be fixed by upgrading.');
+        }
+    }
+
+    addMockEntryFileToConfigEntry(configEntryList, mockEntryAbsPath) {
+        if (configEntryList.includes(mockEntryAbsPath)) {
+            // Mock entry file has already been added to webpack config
+            // Don't add it again if a rebuild is triggered
+            return;
+        }
+
+        configEntryList.push(mockEntryAbsPath);
+    }
+
+    addMockDirToModuleRule(moduleRules, mockDirAbsPath) {
+        const matchingRuleForMockEntryFile = moduleRules.find(rule => {
+            const ruleTest = rule.test;
+
+            if (ruleTest instanceof RegExp) {
+                return ruleTest.test(this.mockEntryFile);
+            } else if (Array.isArray(ruleTest)) {
+                return ruleTest.some(testRegex => testRegex.test(this.mockEntryFile));
+            }
+        });
+
+        if (matchingRuleForMockEntryFile) {
+            const mockDirInclude = matchingRuleForMockEntryFile.include;
+
+            if (mockDirInclude instanceof RegExp) {
+                matchingRuleForMockEntryFile.include = [ mockDirInclude, mockDirAbsPath ];
+            } else if (Array.isArray(mockDirInclude)) {
+                mockDirInclude.push(mockDirAbsPath);
+            }
+        } else {
+            throw new Error(
+                `${this.pluginName}: Could not find a suitable \`module.rule.test\` for ${this.mockEntryFile}.`,
+                `Try using either a RegExp or RegExp[] as a value for \`test\` to ensure proper transpilation of ${this.mocksDir}.`
+            );
+        }
+    }
+
     /**
      * @param {Compiler} compiler
      */
@@ -45,57 +105,8 @@ class MockRequestsWebpackPlugin {
 
         const rules = compiler.options.module.rules;
 
-        compiler.hooks.entryOption.tap(this.constructor.name, (projectRootPath, entry) => {
-            try {
-                const firstEntryName = Object.keys(entry)[0];
-                const firstEntryList = entry[firstEntryName].import;
-                const mockDirAbsPath = this.getAbsPath(projectRootPath);
-                const mockEntryAbsPath = this.getAbsPath(projectRootPath, true);
-
-                if (!mockDirAbsPath) {
-                    throw new Error(`Could not find mock directory "${this.mocksDir}"`);
-                }
-
-                if (!mockEntryAbsPath) {
-                    throw new Error(`Could not find mock entry file "${this.mockEntryFile}"`);
-                }
-
-                if (firstEntryList.includes(mockEntryAbsPath)) {
-                    // Mock entry file has already been added to webpack config
-                    // Don't add it again if a rebuild is triggered
-                    return;
-                }
-
-                firstEntryList.push(mockEntryAbsPath);
-
-                const matchingRuleForMockEntryFile = rules.find(rule => {
-                    const ruleTest = rule.test;
-
-                    if (ruleTest instanceof RegExp) {
-                        return ruleTest.test(this.mockEntryFile);
-                    } else if (Array.isArray(ruleTest)) {
-                        return ruleTest.some(testRegex => testRegex.test(this.mockEntryFile));
-                    }
-                });
-
-                if (matchingRuleForMockEntryFile) {
-                    const mockDirInclude = matchingRuleForMockEntryFile.include;
-
-                    if (mockDirInclude instanceof RegExp) {
-                        matchingRuleForMockEntryFile.include = [ mockDirInclude, mockDirAbsPath ];
-                    } else if (Array.isArray(mockDirInclude)) {
-                        mockDirInclude.push(mockDirAbsPath);
-                    }
-                } else {
-                    throw new Error(
-                        `${this.pluginName}: Could not find a suitable \`module.rule.test\` for ${this.mockEntryFile}.`,
-                        `Try using either a RegExp or RegExp[] as a value for \`test\` to ensure proper transpilation of ${this.mocksDir}.`
-                    );
-                }
-            } catch (e) {
-                console.error('Error:', e.message);
-                console.error('Note:', this.pluginName, 'has only been verified for webpack@>=5. Webpack runtime issues may be fixed by upgrading.');
-            }
+        compiler.hooks.entryOption.tap(this.constructor.name, (context, entry) => {
+            this.injectMocksIntoWebpackConfig(context, rules, entry);
         });
     }
 }
