df4ecc6e7e49d873c321ca14b85e70390e8a61d5
D-Pow <djp460@nyu.edu>
2021-05-15 21:35:42 -0600

    Minor: Remove comments of autocompletion attempts that didn't work for bin/

--- bin/MockRequestsWebpackPlugin.js
+++ bin/MockRequestsWebpackPlugin.js
@@ -2,24 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const { WebpackPluginInstance, Compiler, RuleSetCondition } = require('webpack/lib');
 
-// /** @typedef {import('webpack/types').WebpackPluginInstance} WebpackPluginInstance */
-// /** @typedef {import('webpack/types').Compiler} Compiler */
-// /** @typedef {import('webpack/types').RuleSetCondition} RuleSetCondition */
-
-// TODO multiple externals in one block - is it acceptable?
-/*
- * @external WebpackPluginInstance
- * @see https://github.com/webpack/webpack/blob/master/types.d.ts
- */
-/*
- * @external Compiler
- * @see https://webpack.js.org/api/
- */
-/*
- * @external RuleSetCondition
- * @see https://webpack.js.org/api/
- */
-
 /**
  * Webpack plugin for automatically resolving the mock directory,
  * including transpiling all files it contains as well as adding
