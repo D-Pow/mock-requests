225fee6b4c65c8ed0ffee95668f0c74ed007c7f0
D-Pow <D-Pow@users.noreply.github.com>
2023-03-03 23:21:01 -0700

    Install `mock-requests` from local dir so demo always has latest version installed

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MockRequestsWebpackPlugin = require('mock-requests/bin/MockRequestsWebpackPlugin');
+const MockRequestsWebpackPlugin = require('../dist/bin/MockRequestsWebpackPlugin');
 
 const outputPaths = {
     dev: '',
